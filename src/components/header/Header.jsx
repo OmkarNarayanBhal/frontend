a490700f297a4e65254043df89c0955e74c8b2b5
Omkar Narayan Bhal <115972945+OmkarNarayanBhal@users.noreply.github.com>
2024-08-29 03:32:20 +0530

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Header = ({ isAuth }) => {
   return (
     <header>
-      <div className="logo">Online-Learning platform</div>
+      <div className="logo">E-Learn</div>
 
       <div className="link">
         <Link to={"/"}>Home</Link>
